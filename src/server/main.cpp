dffae8e18fd6cfb3d07745e245ac4752f8521f04
fuyouawa <2907142541@qq.com>
2023-12-22 08:09:44 -0500

    完善main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <csignal>
+#include "server/base/ioservice_pool.h"
 #include "server/chatserver.h"
 
 int main()
@@ -6,8 +8,17 @@ int main()
     try
     {
         boost::asio::io_service ios;
+        auto& pool = chatroom::IOServicePool::instance();
+        pool.Start();
         chatroom::ChatServer server{ios, 8888};
         server.Start();
+
+		boost::asio::signal_set signals(ios, SIGINT, SIGTERM);
+		signals.async_wait([&](auto, auto) {
+			ios.stop();
+            pool.Stop();
+		});
+        
         ios.run();
     }
     catch(const std::exception& e)
